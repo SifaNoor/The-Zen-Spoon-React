a980e9207b319fde815c76395caf62246dbe0d36
Sifatun <sifanoor04@gmail.com>
2025-07-06 00:48:09 -0400

    added hero section

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import React from 'react';
 import './styles/style.css';
 import Header from './components/Header.js';
+import Hero from './components/Hero.js';
 
 function App() {
   return (
     <>
     <Header />
+    <Hero />
     </>
   );
 }
---
+++ src/components/Hero.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Hero() {
+    return (
+    <div id= "home" className ="heroimage">
+        <div className = "herotext">
+            <div className="herologo"> The Zen Spoon</div>
+            <p className = "herocaption"> Experience our authentic Asian cuisine made with traditional values and timeless recipes, all at one location.</p>
+            <button className = "menubutton"> View Menu </button>
+        </div>
+    </div>
+    );
+}
+  
+export default Hero;
